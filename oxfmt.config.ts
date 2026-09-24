093b31ebb95547f42118b94658418024865d2226
Mridu <mridubora785@gmail.com>
2026-09-24 22:16:22 +0530

    Add files via upload

---
+++ oxfmt.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "oxfmt";
+
+export const ignorePatterns = [
+  ".github/**",
+  "src/handlers/index.ts",
+  "test/resources/**",
+  "src/handlers/azw3/**",
+  "src/handlers/libopenmpt/**",
+  "src/handlers/midi/**",
+  "src/handlers/pandoc/**",
+  "src/handlers/envelope/**",
+  "src/handlers/espeakng.js/**",
+  "src/handlers/gimper/**",
+  "src/handlers/image-to-txt/**",
+  "src/handlers/qoa-fu/**",
+  "src/handlers/qoi-fu/**",
+  "src/handlers/rpgmvp-decrypter/**",
+  "src/handlers/sppd/**",
+  "src/handlers/terraria-wld-parser/**",
+  "src/handlers/turbowarp/**",
+];
+
+export default defineConfig({ ignorePatterns });
